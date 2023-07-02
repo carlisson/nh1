a5d7d7b12e0fae68df0dd8a0c9ca1b889ff5b7cb
Cárlisson Bardo <cordeis@vivaldi.net>
2023-07-02 08:40:20 -0300

    created test for 1dw

--- tests/misc_test.sh
+++ tests/misc_test.sh
@@ -11,15 +11,27 @@ testPass() {
   assertNotEquals "1pass not working" "$pass1" "$pass2"
   assertEquals "Password with different sizes" "${#pass1}" "${#pass2}"
   assertTrue "Password too short" "[ ${#pass1} -gt 8 ]"
+  assertTrue "Error message. Check it!" "[ $($NH1 pass | wc -l) -eq 1 ]"
 }
 
 testDiceware() {
   local pass1 pass2
   pass1="$($NH1 diceware)"
   pass2="$($NH1 diceware)"
-  assertNotEquals "1pass not working" "$pass1" "$pass2"
+  assertNotEquals "1diceware not working" "$pass1" "$pass2"
   assertEquals "Password with different sizes" $(echo "$pass1" | wc -w) $(echo "$pass2" | wc -w)
   assertTrue "Password too short" "[ $(echo "$pass1" | wc -w) -gt 3 ]"
+  assertTrue "Error message. Check it!" "[ $($NH1 diceware | wc -l) -eq 1 ]"
+}
+
+testDW() {
+  local pass1 pass2
+  pass1="$($NH1 dw)"
+  pass2="$($NH1 dw)"
+  assertNotEquals "1dw not working" "$pass1" "$pass2"
+  assertEquals "Password with different sizes" $(echo "$pass1" | wc -w) $(echo "$pass2" | wc -w)
+  assertTrue "Password too short" "[ ${#pass1} -gt 5 ]"
+  assertTrue "Error message. Check it!" "[ $($NH1 dw | wc -l) -eq 1 ]"
 }
 
 # Load shUnit2.
