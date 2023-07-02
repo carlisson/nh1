cd825409781411de1390144d6cd7771d7a255a57
Cárlisson Bardo <cordeis@vivaldi.net>
2023-07-02 11:12:10 -0300

    created test for 1pdfopt

--- tests/misc_test.sh
+++ tests/misc_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#cd "$(dirname "${BASH_SOURCE[0]}")"
+TDIR="$(dirname "${BASH_SOURCE[0]}")"
 
 NH1=$(grep nh1 ~/.bashrc | sed 's/source\ //')
 
@@ -26,12 +26,14 @@ testDiceware() {
 
 testDW() {
   local pass1 pass2
-  pass1="$($NH1 dw)"
-  pass2="$($NH1 dw)"
+  pass1="$(timeout 3 $NH1 dw)"
+  assertEquals "1dw in looping" "0" "$?"
+  pass2="$(timeout 3 $NH1 dw)"
+  assertEquals "1dw in looping" 0 $?
   assertNotEquals "1dw not working" "$pass1" "$pass2"
   assertEquals "Password with different sizes" $(echo "$pass1" | wc -w) $(echo "$pass2" | wc -w)
   assertTrue "Password too short" "[ ${#pass1} -gt 5 ]"
-  assertTrue "Error message. Check it!" "[ $($NH1 dw | wc -l) -eq 1 ]"
+  assertTrue "Error message. Check it!" "[ $(timeout 3 $NH1 dw | wc -l) -eq 1 ]"
 }
 
 testColor() {
@@ -40,6 +42,24 @@ testColor() {
   color2="$($NH1 color)"
   assertNotEquals "1color not working" "$color1" "$color2"
   assertTrue "This color is not ok" "[[ $color1 =~ ^[0-9A-Fa-f]{6}$ ]]"
+}
+
+testPdfOpt() {
+  local pdffile="$TDIR/example.pdf"
+  local pdfoptf="$TDIR/example-opt.pdf"
+  echo $pdffile $pdfoptf
+  assertTrue "PDF Example file not found" "[ -f $pdffile ]"
+  if [ -f "$pdfoptf" ]
+  then 
+    rm "$pdfoptf"
+  fi
+  $NH1 pdfopt "$pdffile"
+  assertTrue "Optimized PDF not created" "[ -f $pdfoptf ]"
+  assertTrue "Optimized file is too big" "[ $(stat -c%s $pdffile) -gt $(stat -c%s $pdfoptf) ]"
+  if [ -f "$pdfoptf" ]
+  then
+    rm "$pdfoptf"
+  fi
 }
 
 # Load shUnit2.
